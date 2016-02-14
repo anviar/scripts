7b2cf94d27209b924793190fe6704d1fe5e75ce0
anviar <oleg.kluchkin+github@gmail.com>
2016-02-14 12:01:02 +0300

    Improve CSV-filter
    added checking number of arguments
    sort-column now optional
    added checking  destination path
    added usage help

--- filter_csv_by_column_sort.py
+++ filter_csv_by_column_sort.py
@@ -2,25 +2,37 @@
 
 import os,sys,csv
 
+if len(sys.argv)<3:
+	print "Usage for interactive mode:\n\t%s source_file destination_file" % (sys.argv[0])
+	print "Usage for batch mode:\n\t%s source_file destination_file columns [sort_clumn]" % (sys.argv[0])
+	print "\there:\n\t\tcolumns\t\t- columns, separated by comma, for sample: domainName,contactEmail\n\t\tsort_column\t- optional, by default will be set to first selected column"
+	exit (0)
+
 if not os.path.isfile(sys.argv[1]):
-	print "File not found %s" % sys.argv[1]
+	print "Error: File not found %s" % sys.argv[1]
 	exit(1)
 
 if os.path.exists(sys.argv[2]):
-        print "File already exist %s" % sys.argv[2]
+        print "Error: File already exist %s" % sys.argv[2]
         exit(1)
+if not os.path.isdir(os.path.dirname(sys.argv[2])):
+	print "Error: Destinatin path not exist %s" % os.path.dirname(sys.argv[2])
+	exit (1)
 
 infile=csv.DictReader(open(sys.argv[1], "rb"))
 
 tfields = []
-trow = {}
 tdata = []
 
-try:
+if len(sys.argv)==5:
         for cf in sys.argv[3].split(','):
 		tfields.append(cf)
         csort=sys.argv[4]
-except:
+elif len(sys.argv)==4:
+        for cf in sys.argv[3].split(','):
+                tfields.append(cf)
+	csort=tfields[0]
+elif len(sys.argv)==3:
 	i=0
 	for field in infile.fieldnames:
 		print "%i) %s" % (i,field)
@@ -30,6 +42,9 @@ except:
 		tfields.append(infile.fieldnames[int(cf)])
 	choose = raw_input('Sort by [filed number]: ')
 	csort=infile.fieldnames[int(choose)]
+else:
+	print "Error: wrong number of arguments"
+	exit (1)
 
 for srow in infile:
 	tdata.append ( dict ([ (tfield, srow [tfield]) for tfield in tfields ]) )
