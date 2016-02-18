7178ea5490b67ff19f327b33434eda33ca654808
anviar <oleg.kluchkin+github@gmail.com>
2016-02-18 17:47:04 +0300

    Improve combine script
    added memory limit
    splitting output

--- filter_csv_by_column_sort_combined.py
+++ filter_csv_by_column_sort_combined.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os,sys,csv
+import os,sys,csv,resource,gc
 
 if len(sys.argv)<3:
 	print "Usage for interactive mode:\n\t%s source_file destination_file" % (sys.argv[0])
@@ -18,6 +18,9 @@ if not os.path.isdir(sys.argv[2]):
 
 tfields = []
 tdata = []
+max_memsize = (50 * 1024)
+step_memsize = 256
+last_memsize = 0
 
 if len(sys.argv)==5:
         for cf in sys.argv[3].split(','):
@@ -34,16 +37,33 @@ else:
 for tld in [d for d in os.listdir(sys.argv[1]) if os.path.isdir(sys.argv[1])]:
 	sys.stdout.write("Processing "+tld)
 	sys.stdout.flush()
-	if tld=="com" or os.path.exists(sys.argv[2]+"/"+tld+".csv"):
+	if os.path.exists(sys.argv[2]+"/"+tld+".csv"):
 		print " - skipped"
 		continue
 	del tdata[:]
+	chunk = 0
+	file_count=0
 	for cfile in [f for f in os.listdir(sys.argv[1]+"/"+tld) if f.endswith(".csv")]:
-		sys.stdout.write('.')
-		sys.stdout.flush()
+		#sys.stdout.write('.')
+		#sys.stdout.flush()
+		current_memsize=int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)
+		print (str(file_count) + " mem: " + str(current_memsize) +" Mb")
 		with open(sys.argv[1]+"/"+tld+"/"+cfile, "rb") as wfile:
 			for srow in csv.DictReader(wfile):
 				tdata.append ( dict ([ (tfield, srow [tfield]) for tfield in tfields ]) )
+		if current_memsize > max_memsize and current_memsize > last_memsize: 
+			chunk +=1
+			print "Writing chunk "+ str(chunk)
+			tdata.sort(key=lambda col: col[csort] )
+			with open(sys.argv[2]+"/"+tld+"-"+str(chunk)+".csv","w") as ofile:
+				outfile=csv.DictWriter(ofile,fieldnames=tfields,quoting=csv.QUOTE_ALL)
+				outfile.writerow(dict((fn,fn) for fn in outfile.fieldnames))
+				for row in tdata:
+					outfile.writerow(row)
+			del tdata[:]
+			last_memsize=int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)+step_memsize
+			gc.collect()
+		file_count += 1
 	sys.stdout.write('*')
 	sys.stdout.flush()
 	tdata.sort(key=lambda col: col[csort] )
